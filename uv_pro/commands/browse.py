ba032f77f7d7d2c59fa770dae1b073678993ea8d
David Hebert <39543390+dd-hebert@users.noreply.github.com>
2025-06-02 02:04:36 -0400

    improve prompts

--- uv_pro/commands/browse.py
+++ uv_pro/commands/browse.py
@@ -5,10 +5,16 @@ Functions for the ``browse`` command.
 """
 
 import argparse
+from pathlib import Path
 
 from uv_pro.commands import command
 from uv_pro.commands.process import process
-from uv_pro.utils.filepicker import FilePicker
+from uv_pro.utils.prompts import select
+
+
+def get_files_in_root_dir(root_dir: Path, ext: str = 'KD'):
+    paths = sorted({str(p.relative_to(root_dir)) for p in root_dir.rglob(f'*.{ext}')}, key=str.lower)
+    return paths
 
 
 @command(aliases=['br'])
@@ -20,8 +26,16 @@ def browse(args: argparse.Namespace) -> None:
     *desc : Browse for a .KD file in the root directory and open it in view-only mode.
     *help : Browse for a .KD file in the root directory and open it in view-only mode.
     """
-    if args.root_dir:
-        if file := FilePicker(args.root_dir, '.KD').pick_file():
-            args.path = file[0]
+    if args.root_directory:
+        file = select(
+            'Select a file:',
+            choices=get_files_in_root_dir(Path(args.root_directory)),
+            use_search_filter=True,
+            use_jk_keys=False
+        )
+
+        if file is not None:
+            args.path = file
             args.view = True
+            args.list_colormaps = False
             process(args)
