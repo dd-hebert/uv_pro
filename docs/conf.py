9dca6042fa3740a9f4328a285c0af60d30dabeae
David Hebert <39543390+dd-hebert@users.noreply.github.com>
2025-06-16 21:54:20 -0400

    bump version 0.7.1 -> 0.7.2

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = 'uv_pro'
 copyright = '2023-2025, David Hebert'
 author = 'David Hebert'
-release = '0.7.1'
+release = '0.7.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
